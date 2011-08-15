4a3bba773eaf4685446b38ea82c307eb155b4acd
tokiclover <tokiclover@gmail.com>
2011-08-15 18:55:34 +0000

    updated sqfsd rletaed script, or a rewrite for sqfsd-rebuild.sh with udba=notifyy as default mount option

--- sqfsd/sqfsd-rebuild.sh
+++ sqfsd/sqfsd-rebuild.sh
@@ -4,46 +4,63 @@ BSIZE=131072
 COMP=gzip
 EXCL=$3
 EXT=$2
-sqfsd="`echo $1|tr ':' ' '`"
+sqfsd="$(echo $1|tr ':' ' ')"
 sqfsdir="/sqfsd"
 FSTAB=n
-[ -n "$EXCL" ] && EXCL="`echo $3|tr ':' ' '`" && e="-e $EXCL"
+if [ -n "$EXCL" ]; then EXCL="-e $(echo $3|tr ':' ' ')"; fi
+
 die() {
 	echo "* $1"
 	exit 1
 }
-for s in $sqfsd
+
+for dir in $sqfsd
 do
-   echo ">>> [re]building squashed $s..."
-   mkdir -p "$sqfsdir/$s"/{ro,rw} || die "failed to create $s/{ro,rw} dirs."
-   mksquashfs /$s $sqfsdir/$s.tmp.sfs -b $BSIZE -comp $COMP $e >/dev/null || die "failed to build $s.sfs img."
-   [ "$s" = "lib64" ] && { # move rc-svcdir and cachedir.
-	mkdir -p /var/{lib/init.d,cache/splash}
-	mount --move "/$s/splash/cache" /var/cache/splash || die "fled to move cachedir."
-	mount --move "/$s/rc/init.d" /var/lib/init.d || die "failed to move rc-svcdir."
-	}
-   [ -n "`mount -t aufs|grep -w $s`" ] && { umount -l /$s || die "failed to umount $s aufs branch."; }
-   [ -n "`mount -t squashfs|grep $sqfsdir/$s/ro`" ] && { umount -l $sqfsdir/$s/ro || die "failed to umount sfs img."; }
-   rm -rf "$sqfsdir/$s"/rw/* || die "failed to clean up $sqfdir/$s/rw."
-   [ -e $sqfsdir/$s.sfs ] && rm -f $sqfsdir/$s.sfs 
-   mv $sqfsdir/$s.tmp.sfs $sqfsdir/$s.sfs || die "failed to move $s.tmp.sfs img."
-   # set FSTAB=y if you want localmount to take care of mounting instead of sqfsdmount initservice.
-   if [ "$FSTAB" = "y" ]; then
-   echo "$sqfsdir/$s.sfs   $sqfsdir/$s/ro   squashfs   nodev,loop,ro   0 0" >>/etc/fstab || die "..."
-   echo "$s 	/$s 	aufs 	nodev,udba=reval,br:$sqfsdir/$s/rw:$sqfsdir/$s/ro  0 0" >>/etc/fstab || die "..."
-   fi
-   mount -t squashfs $sqfsdir/$s.sfs $sqfsdir/$s/ro -o nodev,loop,ro || die "failed to mount $s.sfs img."
-   [ -n "$EXT" ] && { # now you can up[date] or rm squashed dir.
-   	case $EXT in
-		rm) rm -rf /$s/*;;
-		up) cp -aru "$sqfsdir/$s"/ro/* /$s;;
-		*) echo "* nothing to do; usage is [up|rm]."
-	esac
-	}
-   mount -t aufs $s /$s -o nodev,udba=reval,br:$sqfsdir/$s/rw:$sqfsdir/$s/ro || die "failed to mount $s aufs branch."
-   [ "$s" = "lib64" ] && { # move back rc-svcdir and cachedir.
-	mount --move /var/cache/splash "/$s/splash/cache" || die "failed to move back cachedir."
-	mount --move /var/lib/init.d "/$s/rc/init.d" || die "failed to move back rc-svcdir."
-	}
-  echo ">>> ...squashed $s sucessfully [re]build."
+	echo ">>> [re]building squashed $dir..."
+	mkdir -p "$sqfsdir/$dir"/{ro,rw} || die "failed to create $dir/{ro,rw} dirs."
+	mksquashfs /$dir $sqfsdir/$dir.tmp.sfs -b $BSIZE -comp $COMP $EXCL >/dev/null || \
+		die "failed to build $dir.sfs img."
+	if [ "$dir" = "lib64" ]; then # move rc-svcdir and cachedir.
+		mkdir -p /var/{lib/init.d,cache/splash}
+		mount --move "/$dir/splash/cache" /var/cache/splash &>/dev/null || die "fled to move cachedir."
+		mount --move "/$dir/rc/init.d" /var/lib/init.d &>/dev/null || die "failed to move rc-svcdir."
+	fi
+	[ -n "$(mount -t aufs|grep -w $dir)" ] && { 
+		umount -l /$dir &>/dev/null || die "failed to umount $dir aufs branch."
+		}
+	[ -n "$(mount -t squashfs|grep $sqfsdir/$dir/ro)" ] && { 
+		umount -l $sqfsdir/$dir/ro &>/dev/null || die "failed to umount sfs img."
+		}
+	rm -rf "$sqfsdir/$dir"/rw/* || die "failed to clean up $sqfdir/$dir/rw."
+	[ -e $sqfsdir/$dir.sfs ] && rm -f $sqfsdir/$dir.sfs 
+	mv $sqfsdir/$dir.tmp.sfs $sqfsdir/$dir.sfs || die "failed to move $dir.tmp.sfs img."
+	if [ "$FSTAB" = "y" ]; then
+		echo "$sqfsdir/$dir.sfs $sqfsdir/$dir/ro squashfs nodev,loop,ro 0 0" >>/etc/fstab || die "..."
+		echo "$dir /$dir aufs nodev,udba=reval,br:$sqfsdir/$dir/rw:$sqfsdir/$dir/ro 0 0" >>/etc/fstab || die "..."
+	fi
+	mount -t squashfs $sqfsdir/$dir.sfs $sqfsdir/$dir/ro -o nodev,loop,ro &>/dev/null || \
+		die "failed to mount $dir.sfs img."
+	if [ -n "$EXT" ]; then # now you can up[date] or rm squashed dir.
+		case $EXT in
+			rm) rm -rf /$dir/*;;
+			up) cp -aru "$sqfsdir/$dir"/ro/* /$dir;;
+			*) echo "* nothing to do, usage is [up|rm].";;
+		esac
+	fi
+	mount -t aufs $dir /$dir -o nodev,udba=reval,br:$sqfsdir/$dir/rw:$sqfsdir/$dir/ro &>/dev/null || \
+		die "failed to mount $dir aufs branch."
+	if [ "$dir" = "lib64" ]; then # move back rc-svcdir and cachedir.
+		mount --move /var/cache/splash "/$dir/splash/cache" &>/dev/nul || die "failed to move back cachedir."
+		mount --move /var/lib/init.d "/$dir/rc/init.d" &>/dev/null || die "failed to move back rc-svcdir."
+	fi
+	echo ">>> ...squashed $dir sucessfully [re]build."
 done
+
+unset BSIZE
+unset COMP
+unset EXCL
+unset EXT
+unset sqfsd
+unset sqfsdir
+unset FSTAB
+
--- sqfsd/sqfsdmount.i
+++ sqfsd/sqfsdmount.i
@@ -21,48 +21,49 @@ depend() {
 
 start() {
 	einfo "mounting system disk-squashed dirs."
-	for D in $SSQFSD; do sqfsd_mount "$SQFSDIR" "$D"; done
+	for DIR in $(echo "$SSQFSD"|tr ':' ' '); do sqfsd_mount "$SQFSDIR" "$DIR"; done
         einfo "mounting local disk-squashed dirs."
-        for D in $LSQFSD; do sqfsd_mount "$SQFSDIR" "$D"; done
+        for DIR in $(echo "$LSQFSD"|tr ':' ' '); do sqfsd_mount "$SQFSDIR" "$DIR"; done
 }
 
 stop() {
 	einfo "umounting local disk-squashed dirs."
-	for D in $LSQFSD; do sqfsd_umount "$SQFSDIR" "$D"; done
+	for DIR in $(echo "$LSQFSD"|tr ':' ' '); do sqfsd_umount "$SQFSDIR" "$DIR"; done
         einfo "remounting in ro mode sys squashed dirs."
-        for D in $SSQFSD; do 
-		if [ "$D" = "usr" ]; then sqfsd_remountro "$SQFSDIR" "$D"
-		elif [ "$D" != "lib64" ]; then sqfsd_umount "$SQFSDIR" "$D"
+        for DIR in $(echo "$SSQFSD"|tr ':' ' '); do 
+		if [ "$DIR" = "usr" ]; then sqfsd_remountro "$SQFSDIR" "$DIR"
+		elif [ "$DIR" != "lib64" ]; then sqfsd_umount "$SQFSDIR" "$DIR"
 		fi
 	done
 }
 
 sqfsd_mount() {
-	if [ -n "`mount -t squashfs|grep "$1/$2"`" ]; then einfo "$1/$2/ro already mounted."
-	else	ebegin "mounting squashed $2.sfs image"
-		mount -t squashfs $1/$2.sfs $1/$2/ro -o nodev,loop,ro &>/dev/null
-       		eend "$?" "mount squashed $2.sfs image failed."
-	fi
-	if [ -n "`mount -t aufs|grep /$2`" ]; then einfo "$2 aufs branch already mounted"
-	else	ebegin "mounting squashed $2 aufs branch"
-		mount -t aufs $2 /$2 -o nodev,udba=reval,br:$1/$2/rw:$1/$2/ro &>/dev/null
-		eend "$?" "mount $2 aufs branch failed."
+	if [ -n "$(mount -t aufs|grep /${2})" ]; then einfo "${2} aufs branch already mounted"
+	else	
+		if [ -n "$(mount -t squashfs|grep ${1}/${2}/ro)" ]; then einfo "${1}/${2}/ro already mounted."
+		else	ebegin "mounting squashed ${2}.sfs image"
+			mount -t squashfs ${1}/${2}.sfs ${1}/${2}/ro -o nodev,loop,ro &>/dev/null
+       			eend "$?" "mount squashed ${2}.sfs image failed."
+		fi
+		ebegin "mounting squashed ${2} aufs branch"
+		mount -t aufs ${2} /${2} -o nodev,udba=notify,reval,br:${1}/${2}/rw:${1}/${2}/ro &>/dev/null
+		eend "$?" "mount ${2} aufs branch failed."
 	fi
 }
 
 sqfsd_umount() {
-        ebegin "umounting squashed $2 aufs branch"
-	umount -lt aufs /$2 -O no_netdev &>/dev/null
-	eend "$?" "umount squashed $2 aufs branch failed."
-	ebegin "umounting squashed $2.sfs image"
-	umount -lt squashfs $1/$2/ro -O no_netdev &>/dev/null
-	eend "$?" "umount squashed $2.sfs image failed."
+        ebegin "umounting squashed ${2} aufs branch"
+	umount -lt aufs /${2} -O no_netdev &>/dev/null
+	eend "$?" "umount squashed ${2} aufs branch failed."
+	ebegin "umounting squashed ${2}.sfs image"
+	umount -lt squashfs ${1}/${2}/ro -O no_netdev &>/dev/null
+	eend "$?" "umount squashed ${2}.sfs image failed."
 }
 
 sqfsd_remountro() {
-	ebegin "mounting in ro imode $2 aufs branch"
-	mount -o remount,ro /$2
-	eend "$?" "failed to mount $2 aufs branch in ro mode."
+	ebegin "mounting in ro imode ${2} aufs branch"
+	mount -o remount,ro /${2} &>/dev/null
+	eend "$?" "failed to mount ${2} aufs branch in ro mode."
 }
 
 # vim:ts=4 
