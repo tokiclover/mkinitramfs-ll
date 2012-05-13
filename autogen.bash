d69d61f64abb0e5ab48a11394bfce56748fa9084
tokiclover <tokiclover@gmail.com>
2012-05-14 00:37:11 +0200

    autogen.{ba,z}sh: updated and 0.6.0 version bump

--- autogen.bash
+++ autogen.bash
@@ -1,39 +1,37 @@
 #!/bin/bash
-# $Id: mkinitramfs-ll/autogen.bash,v 0.5.2.0 2012/05/13 13:54:41 -tclover Exp $
+# $Id: mkinitramfs-ll/autogen.bash,v 0.6.0 2012/05/14 00:36:27 -tclover Exp $
 usage() {
   cat <<-EOF
   usage: ${0##*/} OPTIONS [OPTIONS...]
-  -a|--all                 short forme/hand of '--sqfsd --lvm --gpg --toi'
-  -D|--build               build a static busybox and GnuPG-1.x binaries
-  -f|--font :<font>        append colon separated list of fonts to in include
-  -e|--eversion d          append an extra 'd' version after \$kv to the initramfs image
-  -k|--kversion 3.3.2-git  build an initramfs for '3.1.4-git' kernel, else for \$(uname -r)
-  -c|--comp                compression command to use to build initramfs, default is 'xz -9..'
-  -g|--gpg                 adds GnuPG support, require a static gnupg-1.4.x and 'options.skel'
-  -p|--prefix vmlinuz.     prefix scheme to name the initramfs image default is 'initrd-'
-  -y|--keymap kmx86.bin    append colon separated list of keymaps to include in the initramfs
-  -l|--lvm                 adds LVM2 support, require a static sys-fs/lvm2[static] binary
-  -B|--bindir <bin>        try to include binaries from bin dir {busybox,applets,gpg} first
-  -M|--miscdir <misc>      use msc dir for {.gnupg/gpg.conf,share/gnupg/options.skel} files,
-                           one can add manpages {gpg,lvm,cryptsetup} and user scripts as well
-  -W|--workdir <dir>       working directory where to create initramfs dir, default is PWD
-  -b|--bin :<bin>          append colon separated list of binar-y-ies to include
-  -C|--confdir >dir>       copy gpg.conf, GnuPG configuration file, from dir
-  -m|--mdep :<mod>         colon separated list of kernel module-s to include
-  -s|--splash :<theme>     colon ':' separated list of splash themes to include
-     --mgpg :<mod>         colon separated list of kernel modules to add to gpg group
-     --mboot :<mod>        colon separated list of kernel modules to add to boot group
-     --msqfsd :<mod>       colon separated list of kernel modules to add to sqfsd group
-     --mremdev :<mod>      colon separated list of kernel modules to add to remdev group
-     --mtuxonice :<mod>    colon separated list of kernel modules to add to tuxonice group
-  -t|--toi                 adds tuxonice support for splash, require tuxoniceui_text binary
-  -q|--sqfsd               add aufs(+squashfs modules +{,u}mount.aufs binaries) support
-  -i|--install	           install busybox with symliks to \${opts[--bindir]}, require -b
-  -n|--minimal	           build busybox with minimal applets, default is full applets
-  -r|--raid                add RAID support, copy /etc/mdadm.conf and mdadm binary
-  -U|--ucl-arch i386       ARCH string needed to build busybox linked uClibc
-  -y|--keymap <k:m>        generate <m> keymap using <k> as input keymap
-  -u|--usage               print this help/usage and exit
+  -a, --all                 short forme/hand of '--sqfsd --lvm --gpg --toi'
+  -f, --font [:<font>]      append colon separated list of fonts to in include
+  -e, --eversion d          append an extra 'd' version after \$kv to the initramfs image
+  -k, --kversion 3.3.2-git  build an initramfs for '3.1.4-git' kernel, else for \$(uname -r)
+  -c, --comp                compression command to use to build initramfs, default is 'xz -9..'
+  -g, --gpg                 adds GnuPG support, require a static gnupg-1.4.x and 'options.skel'
+  -p, --prefix initramfs-   prefix scheme to name the initramfs image default is 'initrd-'
+  -y, --keymap :fr-latin1   append colon separated list of keymaps to include in the initramfs
+  -l, --lvm                 adds LVM2 support, require a static sys-fs/lvm2[static] binary
+  -B, --bindir [bin]        try to include binaries from bin dir {busybox,applets,gpg} first
+  -M, --miscdir [misc]      use msc dir for {.gnupg/gpg.conf,share/gnupg/options.skel} files,
+                            one can add manpages {gpg,lvm,cryptsetup} and user scripts as well
+  -W, --workdir [dir]       working directory where to create initramfs dir, default is PWD
+  -b, --bin :<bin>          append colon separated list of binar-y-ies to include
+  -C, --confdir <dir>       copy gpg.conf, GnuPG configuration file, from dir
+  -m, --mdep [:<mod>]       colon separated list of kernel module-s to include
+  -s, --splash :<theme>     colon ':' separated list of splash themes to include
+      --mgpg [:<mod>]       colon separated list of kernel modules to add to gpg group
+      --mboot [:<mod>]      colon separated list of kernel modules to add to boot group
+      --msqfsd [:<mod>]     colon separated list of kernel modules to add to sqfsd group
+      --mremdev [:<mod>]    colon separated list of kernel modules to add to remdev group
+      --mtuxonice [:<mod>]  colon separated list of kernel modules to add to tuxonice group
+  -t, --toi                 adds tuxonice support for splash, require tuxoniceui_text binary
+  -q, --sqfsd               add aufs(+squashfs modules +{,u}mount.aufs binaries) support
+  -i, --install	            install busybox with symliks to \${opts[--bindir]}, require -b
+  -n, --minimal	            build busybox with minimal applets, default is full applets
+  -r, --raid                add RAID support, copy /etc/mdadm.conf and mdadm binary
+  -U, --ucl-arch i386       ARCH string needed to build busybox linked uClibc
+  -u, --usage               print this help/usage and exit
 
   usage: runned without arguments, build an initramfs for kernel \$(uname -r)
   # build an initramfs after building gnupg/busybox (AUFS2/LVM2/GPG support)
@@ -41,10 +39,10 @@ usage() {
 EOF
 exit $?
 }
-opt=$(getopt -o ab:c:e:fgik:lm:p:rs:tuvy:B:M:W:nDC:U:y: -l all,bin:,bindir:,eversion: \
-	  -l gpg,mboot:,mdep:,mgpg:,msqfsd:,mremdev:,mtuxonice:,sqfsd,toi,usage,raid,font: \
-	  -l lvm,miscdir:,workdir:,kversion:,build,confdir:,minimal,ucl-arch:,keymap:,comp: \
-	  -l install,prefix:,splash: -n ${0##*/} -- "$@" || usage)
+opt=$(getopt -o ab:c::e:f::gik::lm::p::rs::tuvy::B::M::W::nDC:U:y:: -l all,bin:,bindir::,eversion: \
+	  -l gpg,mboot::,mdep::,mgpg::,msqfsd::,mremdev::,mtuxonice::,sqfsd,toi,usage,raid,font:: \
+	  -l lvm,miscdir:,workdir:,kversion::,build,confdir:,minimal,ucl-arch:,keymap::,comp:: \
+	  -l install,prefix::,splash:: -n ${0##*/} -- "$@" || usage)
 eval set -- "$opt"
 [[ -z "${opts[*]}" ]] && declare -A opts
 while [[ $# > 0 ]]; do
@@ -56,7 +54,6 @@ while [[ $# > 0 ]]; do
 		-a|--all) opts[sqfsd]=y; opts[gpg]=y; 
 		 opts[lvm]=y; opts[toi]=y; shift;;
 		-s|--sqfsd) opts[sqfsd]=y; shift;;
-		-D|--build) opts[build]=y; shift 2;;
 		-b|--bin) opts[bin]+=:${2}; shift 2;;
 		-c|--comp) opts[comp]="${2}"; shift 2;;
 		-B|--bindir) opts[bindir]=${2}; shift 2;;
@@ -91,12 +88,11 @@ mkdir -p "${opts[workdir]}"
 mkdir -p "${opts[bindir]}"
 error() { echo -ne "\e[1;31m* \e[0m$@\n"; }
 die()   { error "$@"; exit 1; }
-[[ -n "${opts[build]}" ]] && { ./busybox.bash
-	[[ -n "${opts[gpg]}" ]] && { ./gnupg.bash
-		[[ -d "${opts[confdir]}" ]] && { mkdir -p "${opts[miscdir]}"/.gnupg/
-			cp "${opts[confdir]}"/gpg.conf "${opts[miscdir]}"/.gnupg/ || die "eek!"
-		}
-	}
-}
+busybox.bash
+if [[ -n "${opts[gpg]}" ]]; then gnupg.bash
+	if [[ -d "${opts[confdir]}" ]]; then mkdir -p "${opts[miscdir]}"/.gnupg/
+		cp "${opts[confdir]}"/gpg.conf "${opts[miscdir]}"/.gnupg/ || die
+	fi
+fi
 ./mkifs-ll.bash
 # vim:fenc=utf-8:ci:pi:sts=0:sw=4:ts=4:
--- autogen.zsh
+++ autogen.zsh
@@ -1,38 +1,36 @@
 #!/bin/zsh
-# $Id: mkinitramfs-ll/autogen.zsh,v 0.5.2.0 2012/05/13 13:53:47 -tclover Exp $
+# $Id: mkinitramfs-ll/autogen.zsh,v 0.5.2.0 2012/05/14 00:36:30 -tclover Exp $
 usage() {
   cat <<-EOF
   usage: ${(%):-%1x} [OPTIONS] [OPTIONS...]
   -a|-all                 short forme/hand of '-sqfsd -lvm -gpg -toi'
-  -D|-build	           build a static busybox and GnuPG-1.x binaries
-  -f|-font :<font>        append colon separated list of fonts to in include
+  -f|-font [:<font>]      append colon separated list of fonts to in include
   -e|-eversion d          append an extra 'd' version to the initramfs image
   -k|-kversion 3.3.2-git  build an initramfs for '3.3.2-git' kernel, else for \$(uname -r)
   -c|-comp                compression command to use to build initramfs, default is 'xz -9..'
   -g|-gpg                 adds GnuPG support, require a static gnupg-1.4.x and 'options.skel'
-  -p|-prefix vmlinuz.     prefix scheme to name the initramfs image default is 'initrd-'
-  -y|-keymap kmx86.bin    append colon separated list of keymaps to include in the initramfs
+  -p|-prefix initramfs-   prefix scheme to name the initramfs image default is 'initrd-'
+  -y|-keymap [:fr-lati1]  append colon separated list of keymaps to include in the initramfs
   -l|-lvm                 adds LVM2 support, require a static sys-fs/lvm2[static] binary
-  -B|-bindir <bin>        try to include binaries from bin dir (busybox/applets/gpg) first
-  -M|-miscdir <misc>      use msc dir for {.gnupg/gpg.conf,share/gnupg/options.skel} files,
-                           one can add manpages {gpg,lvm,cryptsetup} and user scripts as well
-  -W|-wokdir dir          working directory where to create initramfs dir, default is PWD
+  -B|-bindir [bin]        try to include binaries from bin dir (busybox/applets/gpg) first
+  -M|-miscdir [misc]      use msc dir for {.gnupg/gpg.conf,share/gnupg/options.skel} files,
+                          one can add manpages {gpg,lvm,cryptsetup} and user scripts as well
+  -W|-wokdir [dir]        working directory where to create initramfs dir, default is PWD
   -b|-bin :<bin>          append colon separated list of binar-y-ies to include
   -C|-confdir <dir>       copy gpg.conf, GnuPG configuration file, from dir
-  -m|-mdep :<mod>         colon separated list of kernel module-s to include
-  -s|-splash :<theme>     colon ':' separated list of splash themes to include
-     -mgpg :<mod>         colon separated list of kernel modules to add to gpg group
-     -mboot :<mod>        colon separated list of kernel modules to add to boot group
-     -msqfsd :<mod>       colon separated list of kernel modules to add to sqfsd group
-     -mremdev :<mod>      colon separated list of kernel modules to add to remdev group
-     -mtuxonice :<mod>    colon separated list of kernel modules to add to tuxonice group
+  -m|-mdep [:<mod>]       colon separated list of kernel module-s to include
+  -s|-splash [:<theme>]   colon ':' separated list of splash themes to include
+     -mgpg [:<mod>]       colon separated list of kernel modules to add to gpg group
+     -mboot [:<mod>]      colon separated list of kernel modules to add to boot group
+     -msqfsd [:<mod>]     colon separated list of kernel modules to add to sqfsd group
+     -mremdev [:<mod>]    colon separated list of kernel modules to add to remdev group
+     -mtuxonice [:<mod>]  colon separated list of kernel modules to add to tuxonice group
   -t|-toi                 adds tuxonice support for splash, require tuxoniceui_text binary
   -q|-sqfsd               add aufs(+squashfs modules +{,u}mount.aufs binaries) support
   -i|-install             install busybox with symliks to \${opts[-bindir]}, require -b
   -n|-minimal             build busybox with minimal applets, default is full applets
   -r|-raid                add RAID support, copy /etc/mdadm.conf and mdadm binary
   -U|-ucl-arch i386       ARCH string needed to build busybox linked uClibc
-  -y|-keymap <k:m>        generate <m> keymap using <k> as input keymap
   -u|-usage               print this help/usage and exit
 
   usage: runned without arguments, build an initramfs for kernel \$(uname -r)
@@ -44,29 +42,26 @@ exit 0
 error() { print -P "%B%F{red}*%b%f $@"; }
 die()   { error $@; exit 1; }
 alias die='die "%F{yellow}%1x:%U${(%):-%I}%u:%f" $@'
-if [[ $# = 0 ]] { if [[ -z ${(k)opts[*]} ]] { typeset -A opts }
-	info "initramfs will be build with only LUKS support."
-} else {
-	zmodload zsh/zutil
-	zparseopts -E -D -K -A opts a all q sqfsd g gpg l lvm t toi c: comp: \
-		C: confdir: e: eversion: k: kversion:: m+: mdep+: f+: font+: M: miscdir: \
-		s: splash: u usage W: workdir:  b: bin: p: prefix: y: keymap: B: bindir: \
-		mboot+: mgpg+: mremdev+: msqfsd+: mtuxonice+: r raid D build i install \
-		n minimal y: keymap: U: ucl-arch: || usage
+zmodload zsh/zutil
+zparseopts -E -D -K -A opts a all q sqfsd g gpg l lvm t toi c:: comp:: \
+	C: confdir: e: eversion: k: kversion:: m+: mdep+: f+: font+: M: miscdir: \
+	s: splash: u usage W: workdir:  b: bin: p: prefix: y: keymap: B: bindir: \
+	mboot+: mgpg+: mremdev+: msqfsd+: mtuxonice+: r raid i install n minimal \
+	y: keymap: U: ucl-arch: || usage
 	if [[ $# != 0 ]] || [[ -n ${(k)opts[-u]} ]] || [[ -n ${(k)opts[-usage]} ]] { usage }
-}
+if [[ -z ${(k)opts[*]} ]] { typeset -A opts }
 :	${opts[-workdir]:=${opts[-W]:-$(pwd)}}
 :	${opts[-miscdir]:=${opts[-M]:-${opts[-workdir]}/misc}}
 :	${opts[-bindir]:=${opts[-B]:-${opts[-workdir]}/bin}}
 if [[ -f mkifs-ll.conf.zsh ]] { source mkifs-ll.conf.zsh }
 mkdir -p ${opts[-workdir]}
 mkdir -p ${opts[-bindir]}
-if [[ -n ${(k}opts[-build]} ]] || [[ -n ${(k)opts[-D]} ]] { ./busybox.zsh
-	if [[ -n ${(k}opts[-gpg]} ]] || [[ -n ${(k)opts[-g]} ]] { ./gnupg.zsh
-		if [[ -d ${opts[-confdir]} ]] || [[ -d ${opts[-C]} ]] { 
-			mkdir -p ${opts[-miscdir]}/.gnupg/
-			cp ${opts[-confdir]}/gpg.conf ${opts[-miscdir]}/.gnupg/ || die "eek!"
-		}
+busybox.zsh
+if [[ -n ${(k}opts[-gpg]} ]] || [[ -n ${(k)opts[-g]} ]] { gnupg.zsh
+	if [[ -d ${opts[-confdir]} ]] || [[ -d ${opts[-C]} ]] { 
+		mkdir -p ${opts[-miscdir]}/.gnupg/
+		cp ${opts[-confdir]}/gpg.conf ${opts[-miscdir]}/.gnupg/ || die
+	}
 }
 ./mkifs-ll.zsh
 # vim:fenc=utf-8:ft=zsh:ci:pi:sts=0:sw=4:ts=4:
