042473a21178d64e1915bf5dccc0ef3568e6eef3
tokiclover <tokiclover@gmail.com>
2012-06-05 18:51:44 +0200

    init ChangeLog README.textile busybox.{{ba,z}sh,mcfg}: 0.7.0 version bump

--- busybox.bash
+++ busybox.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
-# $Id: mkinitramfs-ll/busybox.bash,v 0.6.0 2012/05/14 02:39:37 -tclover Exp $
+# $Id: mkinitramfs-ll/busybox.bash,v 0.7.0 2012/06/05 15:36:09 -tclover Exp $
 usage() {
   cat <<-EOF
   usage: ${0##*/} [-m|--minimal] [OPTIONS]
@@ -7,21 +7,23 @@ usage() {
   -n, --minimal             build busybox with minimal applets, default is full applets
   -U, --ucl-arch i386       ARCH string needed to build busybox against uClibc	
   -B, --bindir [bin]        copy builded binary to <bin> directory
+  -v, --version 1.20.0      use 1.20.0 instead of latest version
   -u, --usage               print the usage/help and exit
 EOF
 exit $?
 }
-opt=$(getopt -l install,keymap:,minimal,ucl-arch,usage,bindir: \
-	-o inuDU:B:Y: -n ${0##*/} -- "$@" || usage)
+opt=$(getopt -l bindir:,install,keymap:,minimal,ucl-arch,usage,version: \
+	-o inuDU:B:Y:v: -n ${0##*/} -- "$@" || usage)
 eval set -- "$opt"
 [[ -z "${opts[*]}" ]] && declare -A opts
 while [[ $# > 0 ]]; do
 	case $1 in
-		-i|--intsall) opts[install]=y; shift 2;;
-		-n|--minimal) opts[minimal]=y; shift 2;;
-		-B|--bindir) opts[bindir]="${2}"; shift 2;;
+		-i|--install) opts[install]=y; shift;;
+		-n|--minimal) opts[minimal]=y; shift;;
 		-U|--ucl-arch) opts[U]=${2}; shift 2;;
+		-B|--bindir) opts[bindir]="${2}"; shift 2;;
 		-y|--keymap) opts[keymap]="${2}"; shift 2;;
+		-v|--version) opts[pkg]="=busybox-${2}"; shift 2;;
 		--) shift; break;;
 		-u|--usage|*) usage;;
 	esac
@@ -33,37 +35,19 @@ mkdir -p "${opts[bindir]}"
 error() { echo -ne " \e[1;31m* \e[0m$@\n"; }
 die()   { error "$@"; exit 1; }
 cd "${PORTDIR:-/usr/portage}"/sys-apps/busybox || die
-opts[bbt]=$(emerge -pvO busybox | grep -o "busybox-[-0-9.r]*")
+opts[bbt]=$(emerge -pvO "${opts[pkg]:-busybox}" | grep -o "busybox-[-0-9.r]*")
 ebuild ${opts[bbt]}.ebuild clean || die "clean failed"
 ebuild ${opts[bbt]}.ebuild unpack || die "unpack failed"
 cd "${PORTAGE_TMPDIR:-/var/tmp}"/portage/sys-apps/${opts[bbt]}/work/${opts[bbt]} || die
 if [[ -n "${opts[minimal]}" ]]; then make allnoconfig || die
-	sed -e "s|# CONFIG_INSTALL_NO_USR is not set|CONFIG_INSTALL_NO_USR=y|" \
-	-e "s|CONFIG_FEATURE_SH_IS_NONE=y|# CONFIG_FEATURE_SH_IS_NONE is not set|" \
-	-e "s|# CONFIG_FEATURE_SH_IS_ASH is not set|CONFIG_FEATURE_SH_IS_ASH=y|" \
-	-e "s|# CONFIG_ASH_BUILTIN_ECHO is not set|CONFIG_ASH_BUILTIN_ECHO=y|" \
-	-e "s|# CONFIG_BASENAME is not set|CONFIG_BASENAME=y|" \
-	-e "s|# CONFIG_CAT is not set|CONFIG_CAT=y|" -e "s|# CONFIG_CP is not set|CONFIG_CP=y|" \
-	-e "s|# CONFIG_STATIC is not set|CONFIG_STATIC=y|" -e "s|# CONFIG_MV is not set|CONFIG_MV=y|" \
-	-e "s|# CONFIG_CUT is not set|CONFIG_CUT=y|" -e "s|# CONFIG_FDISK is not set|CONFIG_FDISK=y|" \
-	-e "s|# CONFIG_MKDIR is not set|CONFIG_MKDIR=y|" -e "s|# CONFIG_MKNOD is not set|CONFIG_MKNOD=y|" \
-	-e "s|# CONFIG_FINDFS is not set|CONFIG_FINDFS=y|" -e "s|# CONFIG_FSCK is not set|CONFIG_FSCK=y|" \
-	-e "s|# CONFIG_KBD_MODE is not set|CONFIG_KBD_MODE=y|" -e "s|# CONFIG_ASH is not set|CONFIG_ASH=y|" \
-	-e "s|# CONFIG_INIT is not set|CONFIG_INIT=y|" -e "s|# CONFIG_LOADFONT is not set|CONFIG_LOADFONT=y|" \
-	-e "s|# CONFIG_MODPROBE is not set|CONFIG_MODPROBE=y|" -e "s|# CONFIG_BLKID is not set|CONFIG_BLKID=y|" \
-	-e "s|# CONFIG_RM is not set|CONFIG_RM=y|" -e "s|# CONFIG_BLKID_TYPE is not set|CONFIG_BLKID_TYPE=y|" \
-	-e "s|# CONFIG_MOUNT is not set|CONFIG_MOUNT=y|" -e "s|# CONFIG_RMMOD is not set|CONFIG_RMMOD=y|" \
-	-e "s|# CONFIG_MDEV is not set|CONFIG_MDEV=y|" -e "s|# CONFIG_UMOUNT is not set|CONFIG_UMOUNT=y|" \
-	-e "s|# CONFIG_SED is not set|CONFIG_SED=y|" -e "s|# CONFIG_SETFONT is not set|CONFIG_SETFONT=y|" \
-	-e "s|# CONFIG_HEAD is not set|CONFIG_HEAD=y|" -e "s|# CONFIG_GREP is not set|CONFIG_GREP=y|" \
-	-e "s|# CONFIG_SLEEP is not set|CONFIG_SLEEP=y|" -e "s|# CONFIG_TR is not set|CONFIG_TR=y|" \
-	-e "s|# CONFIG_HEAD is not set|CONFIG_HEAD=y|" -e "s|# CONFIG_TEST is not set|CONFIG_TEST=y|" \
-	-e "s|# CONFIG_TTY is not set|CONFIG_TTY=y|" -e "s|# CONFIG_WHICH is not set|CONFIG_WHICH=y|" \
-	-e "s|# CONFIG_LOADKMAP is not set|CONFIG_LOADKMAP=y|" -e "s|# CONFIG_LN is not set|CONFIG_LN=y|" \
-	-e "s|# CONFIG_SWITCH_ROOT is not set|CONFIG_SWITCH_ROOT=y|" -i .config || die "minimal cfg failed"
-else make defconfig || die "defconfig failed"
+	for cfg in $(< "${opts[workdir]}"/busybox.mcfg); do
+		sed -e "s|# ${cfg%=y} is not set|${cfg}|" -i .config || die
+	done
+else
+	make defconfig || die "defconfig failed"
 	sed -e "s|# CONFIG_STATIC is not set|CONFIG_STATIC=y|" \
-	-e "s|# CONFIG_INSTALL_NO_USR is not set|CONFIG_INSTALL_NO_USR=y|" -i .config || die "config failed"
+		-e "s|# CONFIG_INSTALL_NO_USR is not set|CONFIG_INSTALL_NO_USR=y|" \
+		-i .config || die
 fi
 if [[ -n "${opts[U]}" ]]; then
 	sed -e "s|CONFIG_CROSS_COMPILER_PREFIX=\"\"|CONFIG_CROSS_COMPILER_PREFIX=\"${opts[U]}\"|" \
--- busybox.zsh
+++ busybox.zsh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-# $Id: mkinitramfs-ll/busybox.zsh,v 0.6.0 2012/05/14 00:42:22 -tclover Exp $
+# $Id: mkinitramfs-ll/busybox.zsh,v 0.7.0 2012/06/05 15:36:12 -tclover Exp $
 usage() {
   cat <<-EOF
   usage: ${(%):-%1x} [-m|-minimal] [-U|-ucl-archi386] 
@@ -7,6 +7,7 @@ usage() {
   -n|-minimal             build busybox with minimal applets, default is full applets
   -U|-ucl-arch i386       ARCH string needed to build busybox against uClibc	
   -B|-bindir [<bin>]      copy builded binary to <bin> directory
+  -v|-version 1.20.0      use 1.20.0 instead of latest version
   -u|-usage               print the usage/help and exit
 EOF
 exit $?
@@ -16,7 +17,7 @@ die()   { error $@; exit 1; }
 alias die='die "%F{yellow}%1x:%U${(%):-%I}%u:%f" $@'
 zmodload zsh/zutil
 zparseopts -E -D -K -A opts n minimal i install B: bindir: M: miscdir: \
-	U: ucl-arch: u usage || usage
+	U: ucl-arch: u usage v: version: || usage
 if [[ $# != 0 ]] || [[ -n ${(k)opts[-u]} ]] || [[ -n ${(k)opts[-usage]} ]] { usage }
 if [[ -z ${(k)opts[*]} ]] { typeset -A opts }
 :	${opts[-workdir]:=${opts[-W]:-$(pwd)}}
@@ -26,37 +27,21 @@ if [[ -z ${(k)opts[*]} ]] { typeset -A opts }
 if [[ -f mkifs-ll.conf.zsh ]] { source mkifs-ll.conf.zsh }
 mkdir -p ${opts[-bindir]}
 cd ${PORTDIR:-/usr/portage}/sys-apps/busybox || die
-opts[bbt]=$(emerge -pvO busybox | grep -o "busybox-[-0-9.r]*")
+if [[ -n ${(k)opts[-v]} ]] || [[ -n ${(k)opts[-version]} ]] { 
+	opts[-pkg]="=busybox-${opts[-version]:-${opts[-v]}}"
+} else { opts[-pkg]=busybox }
+opts[bbt]=$(emerge -pvO ${opts[-pkg]} | grep -o "busybox-[-0-9.r]*")
 ebuild ${opts[bbt]}.ebuild clean || die "clean failed"
 ebuild ${opts[bbt]}.ebuild unpack || die "unpack failed"
 cd ${PORTAGE_TMPDIR:-/var/tmp}/portage/sys-apps/${opts[bbt]}/work/${opts[bbt]} || die
 if [[ -n ${(k)opts[-n]} ]] || [[ -n ${(k)opts[-minimal]} ]] { make allnoconfig || die
-	sed -e "s|# CONFIG_INSTALL_NO_USR is not set|CONFIG_INSTALL_NO_USR=y|" \
-	-e "s|CONFIG_FEATURE_SH_IS_NONE=y|# CONFIG_FEATURE_SH_IS_NONE is not set|" \
-	-e "s|# CONFIG_FEATURE_SH_IS_ASH is not set|CONFIG_FEATURE_SH_IS_ASH=y|" \
-	-e "s|# CONFIG_ASH_BUILTIN_ECHO is not set|CONFIG_ASH_BUILTIN_ECHO=y|" \
-	-e "s|# CONFIG_BASENAME is not set|CONFIG_BASENAME=y|" \
-	-e "s|# CONFIG_CAT is not set|CONFIG_CAT=y|" -e "s|# CONFIG_CP is not set|CONFIG_CP=y|" \
-	-e "s|# CONFIG_STATIC is not set|CONFIG_STATIC=y|" -e "s|# CONFIG_MV is not set|CONFIG_MV=y|" \
-	-e "s|# CONFIG_CUT is not set|CONFIG_CUT=y|" -e "s|# CONFIG_FDISK is not set|CONFIG_FDISK=y|" \
-	-e "s|# CONFIG_MKDIR is not set|CONFIG_MKDIR=y|" -e "s|# CONFIG_MKNOD is not set|CONFIG_MKNOD=y|" \
-	-e "s|# CONFIG_FINDFS is not set|CONFIG_FINDFS=y|" -e "s|# CONFIG_FSCK is not set|CONFIG_FSCK=y|" \
-	-e "s|# CONFIG_KBD_MODE is not set|CONFIG_KBD_MODE=y|" -e "s|# CONFIG_ASH is not set|CONFIG_ASH=y|" \
-	-e "s|# CONFIG_INIT is not set|CONFIG_INIT=y|" -e "s|# CONFIG_LOADFONT is not set|CONFIG_LOADFONT=y|" \
-	-e "s|# CONFIG_MODPROBE is not set|CONFIG_MODPROBE=y|" -e "s|# CONFIG_BLKID is not set|CONFIG_BLKID=y|" \
-	-e "s|# CONFIG_RM is not set|CONFIG_RM=y|" -e "s|# CONFIG_BLKID_TYPE is not set|CONFIG_BLKID_TYPE=y|" \
-	-e "s|# CONFIG_MOUNT is not set|CONFIG_MOUNT=y|" -e "s|# CONFIG_RMMOD is not set|CONFIG_RMMOD=y|" \
-	-e "s|# CONFIG_MDEV is not set|CONFIG_MDEV=y|" -e "s|# CONFIG_UMOUNT is not set|CONFIG_UMOUNT=y|" \
-	-e "s|# CONFIG_SED is not set|CONFIG_SED=y|" -e "s|# CONFIG_SETFONT is not set|CONFIG_SETFONT=y|" \
-	-e "s|# CONFIG_HEAD is not set|CONFIG_HEAD=y|" -e "s|# CONFIG_GREP is not set|CONFIG_GREP=y|" \
-	-e "s|# CONFIG_SLEEP is not set|CONFIG_SLEEP=y|" -e "s|# CONFIG_TR is not set|CONFIG_TR=y|" \
-	-e "s|# CONFIG_HEAD is not set|CONFIG_HEAD=y|" -e "s|# CONFIG_TEST is not set|CONFIG_TEST=y|" \
-	-e "s|# CONFIG_TTY is not set|CONFIG_TTY=y|" -e "s|# CONFIG_WHICH is not set|CONFIG_WHICH=y|" \
-	-e "s|# CONFIG_LOADKMAP is not set|CONFIG_LOADKMAP=y|" -e "s|# CONFIG_LN is not set|CONFIG_LN=y|" \
-	-e "s|# CONFIG_SWITCH_ROOT is not set|CONFIG_SWITCH_ROOT=y|" -i .config || die "minimal config failed"
-} else { make defconfig || die "defconfig failed"
+	for cfg ($(< ${opts[-workdir]}/busybox.mcfg))
+	sed -e "s|# ${cfg%'=y'} is not set|${cfg}|" -i .config || die 
+} else {
+	make defconfig || die "defconfig failed" 
 	sed -e "s|# CONFIG_STATIC is not set|CONFIG_STATIC=y|" \
-	-e "s|# CONFIG_INSTALL_NO_USR is not set|CONFIG_INSTALL_NO_USR=y|" -i .config || die "config failed" 
+		-e "s|# CONFIG_INSTALL_NO_USR is not set|CONFIG_INSTALL_NO_USR=y|" \
+		-i .config || die
 }
 if [[ -n ${opts[-U]} ]] || [[ -n ${opts[-ucl-arch]} ]] {
 sed -e "s|CONFIG_CROSS_COMPILER_PREFIX=\"\"|CONFIG_CROSS_COMPILER_PREFIX=\"${opts[-U]}\"|" \
