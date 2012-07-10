edffd00ef239879ba7ed9131f6517791c5914738
tokiclover <tokiclover@gmail.com>
2012-07-10 14:41:37 +0200

    mkinitramfs-ll.d/3d-zfs.sh: updat

--- mkinitramfs-ll.d/3d-zfs.sh
+++ mkinitramfs-ll.d/3d-zfs.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
-# $Id: mkinitramfs-ll.d/3d-zfs.sh, 2012/07/09 -tclover Exp $
+# $Id: mkinitramfs-ll.d/3d-zfs.sh, 2012/07/10 12:11:23 -tclover Exp $
 # to use zfs module one should build an initramfs with `mkinitramfs-ll.$shell ...
-# -mdep spl:znvpair:zcommon:zavl:zunicode:zfs -b hostid:zpool:zfs:zpool_layout
-# -b zpool_id:zvol_id' and `-b :mount.zfs for legacy support arguments.
+# -mdep spl:znvpair:zcommon:zavl:zunicode:zfs -b zpool:zfs
+# and `-b :mount.zfs for legacy support arguments.
 set +e +x
 debug -d -- source /lib/mkinitramfs-ll/functions.sh
-debug _getopt zfs
-[ "$eck" ] && for bin in ${zfs:+mount.zfs} zfs zpool; do 
+[ "$eck" ] && for bin in zfs zpool; do 
 	debug -d -- bck $bin
 done
 echo spl znvpair zcommon zavl zunicode zfs >/etc/mkinitramfs-ll/module.zfs
@@ -20,7 +19,5 @@ if [ -n "$kmode" ]; then
 	kmode=
 fi
 debug -d -- zpool import -N -R /newroot ${dev%/*}
-if [ "$zfs" = "legacy" ]; then
-	debug -d -- mount.zfs $dev /newroot
-else debug -d -- zfs mount $dev; fi
+debug zfs mount $dev || debug -d -- mount.zfs $dev /newroot
 # vim:fenc=utf-8:ft=sh:ci:pi:sts=0:sw=4:ts=4:
