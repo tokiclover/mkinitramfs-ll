c90ee835a98d00e3367b2d1d8f333fc5ea1985a2
tokiclover <tokiclover@gmail.com>
2012-07-11 16:05:51 +0200

    mkinitramfs-ll.d/3d-zfs.sh: (m) update

--- mkinitramfs-ll.d/3d-zfs.sh
+++ mkinitramfs-ll.d/3d-zfs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: mkinitramfs-ll.d/3d-zfs.sh, 2012/07/11 02:42:38 -tclover Exp $
+# $Id: mkinitramfs-ll.d/3d-zfs.sh, 2012/07/11 16:05:33 -tclover Exp $
 # to use zfs module one should build an initramfs with `mkinitramfs-ll.$shell ...
 # -mdep spl:znvpair:zcommon:zavl:zunicode:zfs -b zpool:zfs
 # and `-b :mount.zfs for legacy support arguments.
@@ -18,5 +18,5 @@ if [ -n "$kmode" ]; then
 	kmode=
 fi
 debug -d -- zpool import -N -R /newroot ${dev%/*}
-debug zfs mount $dev || debug -d -- mount.zfs $dev /newroot && rootfs=y
+debug zfs mount $dev || debug -d -- mount.zfs $dev /newroot && export rootfs=mounted
 # vim:fenc=utf-8:ft=sh:ci:pi:sts=0:sw=4:ts=4:
