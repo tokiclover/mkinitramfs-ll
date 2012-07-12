d0d5c7e2e27ef512b30ee1d5ea72342191e27a63
tokiclover <tokiclover@gmail.com>
2012-07-12 15:13:32 +0200

    mkinitramfs-ll.d/3d-zfs.sh: update and fix (stabilized), extra options can be passed as izfs=<arg>,-f

--- mkinitramfs-ll.d/3d-zfs.sh
+++ mkinitramfs-ll.d/3d-zfs.sh
@@ -1,22 +1,26 @@
 #!/bin/sh
-# $Id: mkinitramfs-ll.d/3d-zfs.sh, 2012/07/11 16:05:33 -tclover Exp $
+# $Id: mkinitramfs-ll.d/3d-zfs.sh, 2012/07/12 11:43:11 -tclover Exp $
 # to use zfs module one should build an initramfs with `mkinitramfs-ll.$shell ...
-# -mdep spl:znvpair:zcommon:zavl:zunicode:zfs -b zpool:zfs
-# and `-b :mount.zfs for legacy support arguments.
+# -mdep spl:znvpair:zcommon:zavl:zunicode:zfs -b zpool:zfs and `-b :mount.zfs 
+# for legacy support. and don't forget to append izfs=<arg>[,-f,...] cmdline arg.
 set +e +x
-debug -d -- . /lib/mkinitramfs-ll/functions.sh
+. /lib/mkinitramfs-ll/functions.sh
 [ "$eck" ] && for bin in zfs zpool; do 
 	debug -d -- bck $bin
 done
 debug -d -- _modprobe spl znvpair zcommon zavl zunicode zfs
+dev=${iroot%%:*}
 if [ -n "$kmode" ]; then
-	debug _getopt vdev
-	debug -d -- test -n $vdev
-	for vdev in $(echo "$vdev" | sed 's/:/ /g'); do
+	debug -d -- _getopt izfs
+	debug -d -- test -n "$izfs"
+	for vdev in $(echo "${izfs%%,*}" | sed 's/:/ /g'); do
 		debug -d -- dmopen $vdev
 	done
-	kmode=
 fi
-debug -d -- zpool import -N -R /newroot ${dev%/*}
-debug zfs mount $dev || debug -d -- mount.zfs $dev /newroot && export rootfs=mounted
+opt=$(echo "${izfs#*,}" | sed 's/,/ /g')
+debug -d -- zpool import $opt -N -R /newroot ${dev%/*}
+debug zfs mount $dev || debug -d -- mount.zfs $dev /newroot
+echo unset kfile kmode     >>/run/env
+echo export rootfs=mounted >>/run/env
+unset dev grp izfs vdev
 # vim:fenc=utf-8:ft=sh:ci:pi:sts=0:sw=4:ts=4:
