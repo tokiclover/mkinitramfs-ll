28127be411fe3fabfd18407985c860d3cbbeb07b
tokiclover <tokiclover@gmail.com>
2012-07-30 19:05:51 +0200

    usr/lib/mkinitramfs-ll/init.sh: (m) edit

--- usr/lib/mkinitramfs-ll/init.sh
+++ usr/lib/mkinitramfs-ll/init.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# $Id: mkinitramfs-ll/usr/lib/mkinitramfs-ll/init.sh,v 0.10.8 2012/07/30 18:39:45 -tclover Exp $
+# $Id: mkinitramfs-ll/usr/lib/mkinitramfs-ll/init.sh,v 0.10.8 2012/07/30 19:05:33 -tclover Exp $
 mkdir -p /lib/mkinitramfs-ll/bin
-cat </init | sed '/^#.*$/d' | tail -n+4 | head -n300  >>/lib/mkinitramfs-ll/functions.sh
-sed -e 's/debug\ rsh.*$/exit\ \$_ret/'               -i /lib/mkinitramfs-ll/functions.sh
+cat </init | sed '/^#.*$/d' | tail -n+4 | head -n300 |
+	sed -e 's/debug\ rsh.*$/exit\ \$_ret/' >>/lib/mkinitramfs-ll/functions.sh
 for helper in $(grep '()' /lib/mkinitramfs-ll/functions.sh | sed 's,().*$,,g'); do
 	{
 		echo '#!/bin/sh'
@@ -11,3 +11,5 @@ for helper in $(grep '()' /lib/mkinitramfs-ll/functions.sh | sed 's,().*$,,g'); 
 	} >/lib/mkinitramfs-ll/bin/$helper
 	chmod +x /lib/mkinitramfs-ll/bin/$helper
 done
+# vim:fenc=utf-8:ft=sh:ci:pi:sts=0:sw=4:ts=4:
+
