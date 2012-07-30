5890b777fe916307f3594b9152a81bde51b51b6b
tokiclover <tokiclover@gmail.com>
2012-07-30 18:40:06 +0200

    usr/lib/mkinitramfs-ll/init.sh: (m) fix

--- usr/lib/mkinitramfs-ll/init.sh
+++ usr/lib/mkinitramfs-ll/init.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# $Id: mkinitramfs-ll/usr/lib/mkinitramfs-ll/init.sh,v 0.10.8 2012/07/30 14:05:13 -tclover Exp $
+# $Id: mkinitramfs-ll/usr/lib/mkinitramfs-ll/init.sh,v 0.10.8 2012/07/30 18:39:45 -tclover Exp $
 mkdir -p /lib/mkinitramfs-ll/bin
 cat </init | sed '/^#.*$/d' | tail -n+4 | head -n300  >>/lib/mkinitramfs-ll/functions.sh
 sed -e 's/debug\ rsh.*$/exit\ \$_ret/'               -i /lib/mkinitramfs-ll/functions.sh
@@ -7,7 +7,7 @@ for helper in $(grep '()' /lib/mkinitramfs-ll/functions.sh | sed 's,().*$,,g'); 
 	{
 		echo '#!/bin/sh'
 		echo 'source /lib/mkinitramfs-ll/functions.sh'
-		echo '$0 "$@"'
+		echo '${0##*/} "$@"'
 	} >/lib/mkinitramfs-ll/bin/$helper
 	chmod +x /lib/mkinitramfs-ll/bin/$helper
 done
