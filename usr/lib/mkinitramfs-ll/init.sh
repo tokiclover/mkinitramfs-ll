ed2db1b2aa448be786e97cb7788b2437d6f4e4db
tokiclover <tokiclover@gmail.com>
2012-07-30 14:16:57 +0200

    usr/lib/mkinitramfs-ll/init.sh: added this little script that take care of building fbase.sh (base functions) and build helper which can be used in scripts/modules by adding "export PATH=$PATH:/lib/mkinitramfs-ll/bin" instead of "source /lib/mkinitramfs-ll/fbase.sh"

---
+++ usr/lib/mkinitramfs-ll/init.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# $Id: mkinitramfs-ll/usr/lib/mkinitramfs-ll/init.sh,v 0.10.8 2012/07/30 14:05:13 -tclover Exp $
+mkdir -p /lib/mkinitramfs-ll/bin
+cat </init | sed '/^#.*$/d' | tail -n+4 | head -n300  >>/lib/mkinitramfs-ll/functions.sh
+sed -e 's/debug\ rsh.*$/exit\ \$_ret/'               -i /lib/mkinitramfs-ll/functions.sh
+for helper in $(grep '()' /lib/mkinitramfs-ll/functions.sh | sed 's,().*$,,g'); do
+	{
+		echo '#!/bin/sh'
+		echo 'source /lib/mkinitramfs-ll/functions.sh'
+		echo '$0 "$@"'
+	} >/lib/mkinitramfs-ll/bin/$helper
+	chmod +x /lib/mkinitramfs-ll/bin/$helper
+done
