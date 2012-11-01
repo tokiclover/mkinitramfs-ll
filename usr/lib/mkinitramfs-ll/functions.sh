fb3defcea3155658f5e9f36f23118d508bfb39a8
tokiclover <tokiclover@gmail.com>
2012-11-01 13:24:41 +0100

    init usr/lib/mkinitramfs-ll/{functions,init}.sh: 0.11.2 version bump

--- usr/lib/mkinitramfs-ll/functions.sh
+++ usr/lib/mkinitramfs-ll/functions.sh
@@ -1,11 +1,3 @@
-# $Id: /lib/mkinitramfs-ll/functions.sh,v 0.10.8 2012/07/30 13:59:49 -tclover Exp $
+# $Id: /lib/mkinitramfs-ll/functions.sh,v 0.11.2 2012/11/01 00:27:33 -tclover Exp $
 # vim:fenc=utf-8:ft=sh:ci:pi:sts=0:sw=4:ts=4:
-
-_getopt() {
-	for arg in $*; do
-		for _opt in $(cat /proc/cmdline); do
-			[ "$arg" = "${_opt%%=*}" ] && export $_opt && break
-		done
-	done
-}
 
