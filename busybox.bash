24985360f0b24bae340d5f77d88324bdd2a467ca
tokiclover <tokiclover@gmail.com>
2013-04-11 10:40:45 +0200

    busybox.{ba,z}sh: 0.12.0 version bump

--- busybox.bash
+++ busybox.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
-# $Id: mkinitramfs-ll/busybox.bash,v 0.11.1 2012/10/16 21:54:55 -tclover Exp $
+# $Id: mkinitramfs-ll/busybox.bash,v 0.12.0 2013/04/11 10:40:18 -tclover Exp $
+
+# @FUNCTION: usage
+# @DESCRIPTION: print usages message
 usage() {
   cat <<-EOF
  usage: ${0##*/}[-m|--minimal] [--ucl=i386]
@@ -12,9 +15,23 @@ usage() {
 EOF
 exit $?
 }
+
+# @FUNCTION: error
+# @DESCRIPTION: print error message to stdout
+error() {
+	echo -ne " \e[1;31m* \e[0m$@\n"
+}
+# @FUNCTION: die
+# @DESCRIPTION: call error() to print error message before exiting
+die() {
+	error "$@"
+	exit 1
+}
+
 opt=$(getopt -l usrdir:,minimal,ucl:,usage,version: -o nud::v: \
 	-n ${0##*/} -- "$@" || usage)
 eval set -- "$opt"
+
 declare -A opts
 while [[ $# > 0 ]]; do
 	case $1 in
@@ -22,41 +39,62 @@ while [[ $# > 0 ]]; do
 		--ucl) opts[-ucl]=${2}; shift 2;;
 		-d|--usrdir) opts[-usrdir]="${2}"; shift 2;;
 		-y|--keymap) opts[-keymap]="${2}"; shift 2;;
-		-v|--version) opts[-pkg]="=busybox-${2}"; shift 2;;
+		-v|--version) opts[-version]="${2}"; shift 2;;
 		--) shift; break;;
 		-u|--usage|*) usage;;
 	esac
 done
+
 [[ -f mkinitramfs-ll.conf ]] && source mkinitramfs-ll.conf ||
 	die "no mkinitramfs-ll.conf found"
+
+# @VARIABLE: opts[-workdir]
+# @DESCRIPTION: initial working directory, where to build everythng
 [[ -n "${opts[-workdir]}" ]] || opts[-workdir]="$(pwd)"
 [[ -n "${opts[-usrdir]}" ]] || opts[-usrdir]="${opts[-workdir]}"/usr
+# @VARIABLE: opts[-version]
+# @DESCRIPTION: GnuPG version to build
+#
+# @VARIABLE: opts[-pkg]
+# @DESCRIPTION: busybox version to build
+opts[-pkg]=busybox
+
+if [[ -n ${opts[-version]} ]]; then 
+:	opts[-pkg]=${opts[-pkg]}-${opts[-version]}
+else
+	opts[-pkg]=$(emerge -pvO ${opts[-pkg]} | grep -o "busybox-[-0-9.r]*")
+fi
+
 mkdir -p "${opts[-usrdir]}"/bin
-error() { echo -ne " \e[1;31m* \e[0m$@\n"; }
-die()   { error "$@"; exit 1; }
+
 pushd "${PORTDIR:-/usr/portage}"/sys-apps/busybox || die
-opts[-pkg]=$(emerge -pvO "${opts[-pkg]:-busybox}" | grep -o "busybox-[-0-9.r]*")
 ebuild ${opts[-pkg]}.ebuild clean || die "clean failed"
 ebuild ${opts[-pkg]}.ebuild unpack || die "unpack failed"
 pushd "${PORTAGE_TMPDIR:-/var/tmp}"/portage/sys-apps/${opts[-pkg]}/work/${opts[-pkg]} || die
+
 if [[ -n "${opts[-minimal]}" ]]; then make allnoconfig || die
 	while read cfg; do
 		sed -e "s|# ${cfg%'=y'} is not set|${cfg}|" -i .config || die 
-	done < "${opts[-workdir]}"/busybox.cfg
+	done <"${opts[-workdir]}"/busybox.cfg
 else
 	make defconfig || die "defconfig failed"
 	sed -e "s|# CONFIG_STATIC is not set|CONFIG_STATIC=y|" \
 		-e "s|# CONFIG_INSTALL_NO_USR is not set|CONFIG_INSTALL_NO_USR=y|" \
 		-i .config || die
 fi
+
 if [[ -n "${opts[-ucl]}" ]]; then
 	sed -e "s|CONFIG_CROSS_COMPILER_PREFIX=\"\"|CONFIG_CROSS_COMPILER_PREFIX=\"${opts[-ucl]}\"|" \
 		-i .config || die "setting uClib ARCH failed"
 fi
+
 make || die "failed to build busybox"
 cp -a busybox "${opts[-usrdir]}"/bin/ || die
+
 popd || die
 ebuild ${opts[-pkg]}.ebuild clean || die
 popd || die
+
 unset -v opts[-pkg] opts[-minimal] opts[-ucl]
+
 # vim:fenc=utf-8:ci:pi:sts=0:sw=4:ts=4:
--- busybox.zsh
+++ busybox.zsh
@@ -1,5 +1,8 @@
 #!/bin/zsh
-# $Id: mkinitramfs-ll/busybox.zsh,v 0.11.1 2012/10/16 21:54:49 -tclover Exp $
+# $Id: mkinitramfs-ll/busybox.zsh,v 0.12.0 2013/04/11 10:40:15 -tclover Exp $
+
+# @FUNCTION: usage
+# @DESCRIPTION: print usages message
 usage() {
   cat <<-EOF
  usage: ${(%):-%1x} [-m|-minimal] [-ucl i386]
@@ -12,27 +15,54 @@ usage() {
 EOF
 exit $?
 }
-error() { print -P " %B%F{red}*%b%f $@"; }
-die()   { error $@; exit 1; }
+
+# @FUNCTION: error
+# @DESCRIPTION: print error message to stdout
+error() { print -P " %B%F{red}*%b%f $@" }
+# @FUNCTION: die
+# @DESCRIPTION: call error() to print error message before exiting
+die()   { error $@; exit 1 }
 alias die='die "%F{yellow}%1x:%U${(%):-%I}%u:%f" $@'
+
 zmodload zsh/zutil
 zparseopts -E -D -K -A opts n minimal d:: usrdir:: ucl: u usage v: version: || usage
+
 if [[ -n ${(k)opts[-u]} ]] || [[ -n ${(k)opts[-usage]} ]] { usage }
+
 if [[ $# < 1 ]] { typeset -A opts }
+
 if [[ -f mkinitramfs-ll.conf ]] { source mkinitramfs-ll.conf 
 } else { die "no mkinitramfs-ll.conf found" }
+
+# @VARIABLE: opts[-workdir]
+# @DESCRIPTION: initial working directory, where to build everythng
 :	${opts[-workdir]:=${opts[-W]:-$(pwd)}}
+# @VARIABLE: opts[-usrdir]
+# @DESCRIPTION: usr dir path, to get extra files
 :	${opts[-usrdir]:=${opts[-d]:-$opts[-workdir]/usr}}
-mkdir -p ${opts[-usrdir]}/bin
-pushd ${PORTDIR:-/usr/portage}/sys-apps/busybox || die
+# @VARIABLE: opts[-version] | opts[-v]
+# @DESCRIPTION: GnuPG version to build
+#
+# @VARIABLE: opts[-pkg]
+# @DESCRIPTION: busybox package to build
+opts[-pkg]=busybox
+
 if [[ -n ${(k)opts[-v]} ]] || [[ -n ${(k)opts[-version]} ]] { 
-:	opts[-pkg]="=busybox-${opts[-version]:-${opts[-v]}}"
-} else { opts[-pkg]=busybox }
-opts[-pkg]=$(emerge -pvO ${opts[-pkg]} | grep -o "busybox-[-0-9.r]*")
+:	opts[-pkg]=${opts[-pkg]-${opts[-version]:-${opts[-v]}}
+} else {
+	opts[-pkg]=$(emerge -pvO ${opts[-pkg]} | grep -o "busybox-[-0-9.r]*")
+}
+
+mkdir -p ${opts[-usrdir]}/bin
+
+pushd ${PORTDIR:-/usr/portage}/sys-apps/busybox || die
+
 ebuild ${opts[-pkg]}.ebuild clean || die "clean failed"
 ebuild ${opts[-pkg]}.ebuild unpack || die "unpack failed"
 pushd ${PORTAGE_TMPDIR:-/var/tmp}/portage/sys-apps/${opts[-pkg]}/work/${opts[-pkg]} || die
-if [[ -n ${(k)opts[-n]} ]] || [[ -n ${(k)opts[-minimal]} ]] { make allnoconfig || die
+
+if [[ -n ${(k)opts[-n]} ]] || [[ -n ${(k)opts[-minimal]} ]] {
+	make allnoconfig || die
 	for cfg ($(< ${opts[-workdir]}/busybox.cfg))
 	sed -e "s|# ${cfg%'=y'} is not set|${cfg}|" -i .config || die 
 } else {
@@ -41,14 +71,19 @@ if [[ -n ${(k)opts[-n]} ]] || [[ -n ${(k)opts[-minimal]} ]] { make allnoconfig |
 		-e "s|# CONFIG_INSTALL_NO_USR is not set|CONFIG_INSTALL_NO_USR=y|" \
 		-i .config || die
 }
+
 if [[ -n ${opts[-ucl]} ]] {
-sed -e "s|CONFIG_CROSS_COMPILER_PREFIX=\"\"|CONFIG_CROSS_COMPILER_PREFIX=\"${opts[-ucl]}\"|" \
+	sed -e "s|CONFIG_CROSS_COMPILER_PREFIX=\"\"|CONFIG_CROSS_COMPILER_PREFIX=\"${opts[-ucl]}\"|" \
 	-i .config || die "setting uClib ARCH failed"
 }
+
 make || die "failed to build busybox"
 cp -a busybox ${opts[-usrdir]}/bin || die
 popd || die
+
 ebuild ${opts[-pkg]}.ebuild clean || die
 popd || die
+
 unset opts[-pkg] opts[-n] opts[-minimal] opts[-ucl]
+
 # vim:fenc=utf-8ft=zsh:ci:pi:sts=0:sw=4:ts=4:
