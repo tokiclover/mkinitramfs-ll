f99d15fc193a29a055fa34ea9dc99302e2586e19
tclover <tokiclover@gmail.com>
2015-01-17 10:12:39 +0100

    scripts/busybox.{ba,z}sh: minor fix (minimal.config)

--- scripts/busybox.bash
+++ scripts/busybox.bash
@@ -105,7 +105,7 @@ if [[ "${opts[-minimal]}" ]]; then
 	make allnoconfig || die
 	while read cfg; do
 		sed -e "s|# ${cfg%'=y'} is not set|${cfg}|" -i .config || die 
-	done <"${0%/*}"/busybox-minimal.config
+	done <"${opts[-usrdir]}"/../scripts/minimal.config
 else
 	make defconfig || die "defconfig failed"
 	sed -e "s|# CONFIG_STATIC is not set|CONFIG_STATIC=y|" \
