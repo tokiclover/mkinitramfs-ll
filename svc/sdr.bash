02e1b32c5af5b32daea466caa664981ccde8c393
tclover <tokiclover@gmail.com>
2015-05-29 12:11:14 +0200

    svc/sdr.bash: Added OverlayFS support and minor refactorization (0.21.0 version bump)

--- svc/sdr.bash
+++ svc/sdr.bash
@@ -3,7 +3,7 @@
 # $Header: mkinitramfs-ll/svc/sdr.bash                   Exp $
 # $Author: (c) 2011-2015 -tclover <tokiclover@gmail.com> Exp $
 # $License: 2-clause/new/simplified BSD                  Exp $
-# $Version: 0.20.0 2015/05/24 12:33:03                   Exp $
+# $Version: 0.21.0 2015/05/28 12:33:03                   Exp $
 #
 
 shopt -qs extglob
@@ -11,8 +11,9 @@ typeset -A PKG
 PKG=(
 	[name]=sdr
 	[shell]=bash
-	[version]=0.20.0
+	[version]=0.21.0
 )
+NULL=/dev/null
 
 # @FUNCTION: Print help message
 function usage {
@@ -20,7 +21,8 @@ function usage {
   ${PKG[name]}.${PKG[shell]} version ${PKG[version]}
   usage: ${PKG[name]}.${PKG[shell]} [options] <directory-ies>
 
-  -q, --squash-root=<dir>   Set root directory (default '/aufs')
+  -f, --filesystem=aufs     Set the merge-filesystem to use
+  -q, --squash-root=<dir>   Set root directory (default '/squash')
   -b, --block-size=131072   Set block size in bytes (default 128KB)
   -x, --busybox=busybox     Static BusyBox to use (System Wide case)
   -c, --compressor=gzip     Set compressor to use (default to lzo)
@@ -28,7 +30,7 @@ function usage {
   -o, --offset=0            Offset to use when rebuilding (default 10%)
   -u, --update              Update the underlying source directory
   -r, --remove              Remove the underlying source directory
-  -n, --no-remount          Disable mount after rebuild or update
+  -n, --no-mount            Disable mount after rebuild or update
   -h, --help, -?            Print this help message and exit
 EOH
 exit $?
@@ -40,9 +42,9 @@ exit $?
 declare -A opts
 declare -a opt
 opt=(
-	"-o" "?b:c:o:nhruq:X:x::"
-	"-l" "block-size:,busybox::,compressor:,exclude:,offset,help"
-	"-l" "no-remount,squash-root:,remove,update"
+	"-o" "?b:c:f:o:nhruq:X:x::"
+	"-l" "block-size:,busybox::,compressor:,exclude:,filesystem:,offset,help"
+	"-l" "no-mount,squash-root:,remove,update"
 	"-n" "${PKG[name]}.${PKG[shell]}"
 	"-s" "${PKG[shell]}"
 )
@@ -50,12 +52,15 @@ opt=($(getopt "${opt[@]}" -- "$@" || usage))
 eval set -- "${opt[@]}"
 
 while true; do
-	case $1 in
+	case "$1" in
 		(-b|--block-size)
-			opts[-bsize]="$2"
+			opts[-block-size]="$2"
 			shift 2;;
 		(-x|--busybox)
 			opts[-busybox]="${2:-$commands[busybox]}"
+			shift 2;;
+		(-f|--filesystem)
+			opts[-filesystem]="$2"
 			shift 2;;
 		(-o|--offset)
 			opts[-offset]="$2"
@@ -63,7 +68,7 @@ while true; do
 		(-X|--exclude) opts[-exclude]+=":$2"
 			shift 2;;
 		(-q|--squash-root)
-			opts[-root]="$2"
+			opts[squash-root]="$2"
 			shift 2;;
 		(-c|--compressor)
 			opts[-compressor]="$2"
@@ -74,7 +79,7 @@ while true; do
 		(-r|--remove)
 			opts[-remove]=true
 			shift;;
-		(-n|--no-remount)
+		(-n|--no-mount)
 			opts[-mount]=false
 			shift;;
 		(--)
@@ -97,87 +102,100 @@ function error {
 function die {
 	local ret=$?
 	error "$@"
-	return $ret
+	exit $ret
 }
 
-# @VARIABLE: Kernel bit lenght
-opts[-arc]=$(getconf LONG_BIT)
 # @VARIABLE: Root directory (mount hierarchy)
-[[ "${opts[-root]}" ]] || opts[-root]=/aufs
-[[ "${opts[-bsize]}" ]] || opts[-bsize]=131072
+:	${opts[squash-root]:=/squash}
+:	${opts[-block-size]:=131072}
 # @VARIABLE: Full path to a static busysbox (required for system update)
-[[ "${opts[-busybox]}" ]] || opts[-busybox]="$(type -p busybox)"
+:	${opts[-busybox]:=$(type -p busybox 2>${NULL})}
 # @VARIABLE: Compression command
-[[ "${opts[-comp]}" ]] || opts[-comp]="lzo -Xcompression-level 1"
-# @VARIABLE: Colon separated list of excluded directory
-[[ "${opts[-exclude]}" ]] && opts[-exclude]="-wildcards -regex -e ${opts[-exclude]//:/ }"
+:	${opts[-compressor]:=lzo -Xcompression-level 1}
 # @VARIABLE: rw/rr branch ration (percent)
-[[ "${opts[-offset]}" ]] || opts[-offset]=10
+:	${opts[-offset]:=10}
 
 # @FUNCTION: Helper to mount squashed directory
 function squash-mount {
-	if [[ "${dir}" == *(s)bin || "${dir}" == *lib*(32|64) ]]; then
-		ldd ${opts[-busybox]} >/dev/null && die "no static busybox binary found"
-		local busybox=/tmp/busybox
-		cp ${opts[-busybox]} ${busybox} || die
-		local cp="${busybox} cp -a"        mv="${busybox} mv"
-		local rm="${busybox} rm -fr"    mount="${busybox} mount"
-		local umount="${busybox} umount" grep="${busybox} grep"
-		local mkdir="${busybox} mkdir -p"
-	else
-		local cp="cp -a" grep=grep mv=mv rm="rm -fr"
-		local mount=mount umount=umount
-		local mkdir="mkdir -p"
-	fi
+	local cp grep mv rm mount umount mkdir
+	case "${dir}" in
+		(/bin|/sbin|/lib32|/lib64)
+		ldd ${busybox} >${NULL} 2>&1 && die "No static busybox binary found"
+		cp ${busybox} /tmp && busybox=/tmp/busybox || die
+		cp="${busybox} cp -a" mv="${busybox} mv" rm="${busybox} rm -fr"
+		mount="${busybox} mount" umount="${busybox} umount"
+		grep="${busybox} grep" mkdir="${busybox} mkdir -p -m 0755"
+		;;
+		(*)
+		cp="cp -a" grep=grep mv=mv rm="rm -fr"
+		mount=mount umount=umount mkdir="mkdir -p -m 0755"
+		;;
+	esac
 
-	if ${grep} -q aufs:${dir} /proc/mounts; then
-		auplink ${dir} flush
-		${umount} -l aufs:${dir} || die "failed to umount aufs:${dir}"
+	if ${grep} -q ${opts[-filesystem]}:${dir} /proc/mounts; then
+		case "${opts[-filesystem]}" in
+			(aufs) auplink ${dir} flush >${NULL} 2>&1;;
+		esac
+		${umount} -l ${opts[-filesystem]}:${dir} >${NULL} 2>&1 ||
+			die "Failed to umount ${opts[-filesystem]}:${dir}"
 	fi
 	if ${grep} -q ${DIR}/rr /proc/mounts; then
-		${umount} -l ${DIR}/rr || die "failed to umount ${DIR}.squashfs"
+		${umount} -l ${DIR}/rr >${NULL} 2>&1 ||
+			die "Failed to umount ${DIR}.squashfs"
 	fi
-	${rm} "${DIR}"/rw/* || die "failed to clean up ${DIR}/rw"
+	${rm} "${DIR}"/rw/* || die "Failed to clean up ${DIR}/rw"
 
 	[[ -e ${DIR}.squashfs ]] && [[ -e ${DIR}.tmp.squashfs ]] &&
-	${rm} ${DIR}.squashfs
+		${rm} ${DIR}.squashfs
 	${mv} ${DIR}.tmp.squashfs ${DIR}.squashfs ||
-	die "failed to move ${dir}.tmp.squashfs"
+		die "Failed to move ${dir}.tmp.squashfs"
 
-	if ${mount} -t squashfs -o nodev,loop,ro ${DIR}.squashfs ${DIR}/rr; then
-		if [[ "${opts[-remove]}" ]]; then
-			${rm} ${dir} && $mkdir ${dir} || die "failed to clean up ${dir}"
-		fi
-		if [[ "${opts[-update]}" ]]; then
-			${rm} ${dir} && $mkdir ${dir} && ${cp} ${DIR}/rr ${dir} ||
-			die "failed to update ${dir}"
-		fi
-		${mount} -t aufs -o nodev,udba=reval,br:${DIR}/rw:${DIR}/rr aufs:${dir} ${dir} ||
-		die "failed to mount aufs:${dir} branch"
-	else
-		die "failed to mount ${DIR}.squashfs"
+	${mount} -t squashfs -o nodev,loop,ro ${DIR}.squashfs ${DIR}/rr >${NULL} 2>&1 ||
+		die "Failed to mount ${DIR}.squashfs"
+	if [[ "${opts[-remove]}" ]]; then
+		${rm} ${dir} && ${mkdir} ${dir} || die "Failed to clean up ${dir}"
 	fi
+	if [[ "${opts[-update]}" ]]; then
+		${rm} ${dir} && ${mkdir} ${dir} && ${cp} ${DIR}/rr ${dir} >${NULL} 2>&1 ||
+			die "Failed to update ${dir}"
+	fi
+	${mount} -t ${opts[-filesystem]} -o ${opt} \
+		${opts[-filesystem]}:${dir} ${dir} >${NULL} 2>&1 ||
+		die "Failed to mount ${opts[-filesystem]}:${dir}"
 }
 # @FUNCTION: Helper to squash-directory
 function squash-dir {
-	mkdir -p -m 0755 "${DIR}"/{rr,rw} || die "failed to create ${DIR}/{rr,rw} dirs"
-	mksquashfs ${dir} ${DIR}.tmp.squashfs -b ${opts[-bsize]} -comp ${opts[-comp]} \
-		${opts[-exclude]} || die "failed to build ${dir}.squashfs img"
-	[[ "${opts[-mount]}" ]] || squash-mount
+	case "${opts[-filesystem]}" in
+		(aufs)
+			opt=nodev,udba=reval,br:${DIR}/rw:${DIR}/rr
+			mkdir -p -m 0755 ${DIR}/r{r,w};;
+		(overlay)
+			opt=nodev,upperdir=${DIR}/up,lowerdir=${DIR}/rr,workdir=${DIR}/wk
+			mkdir -p -m 0755 ${DIR}/{rr,up,wk};;
+	esac
+	(( ${?} == 0 )) || die "Failed to create required directories"
+
+	mksquashfs ${dir} ${DIR}.tmp.squashfs -b ${opts[-block-size]} -comp ${opts[-compressor]} \
+		${opts[-exclude]:+-wildcards -regex -e} ${opts[-exclude]} ||
+		die "Failed to build ${dir}.squashfs image"
 	echo ">>> ${0##*/}: ...sucessfully build squashed"
+
+	[[ "${opts[-mount]}" ]] || squash-mount
 }
 
-for mod in aufs squashfs; do
-	grep -q ${mod} /proc/filesystems || modprobe ${mod} >/dev/null 2>&1 ||
-	case ${mod} in
-		(aufs) warn "Failed to load ${mod} module"; opts[-mount]=false;;
-		(s*fs) die  "Failed to load ${mod} module";;
+for mod in ${opts[-filesystem]:-aufs overlay} squashfs; do
+	grep -q ${mod} /proc/filesystems || modprobe ${mod} >${NULL} 2>&1 ||
+		case ${mod} in
+			(aufsi|overlay) error "Failed to load ${mod} module"; opts[-mount]=false;;
+			(squashfs) die  "Failed to load ${mod} module";;
+		esac
+	case "${mod}" in
+		(aufs|overlay) opts[-filesystem]="${mod}";;
 	esac
 done
 
-IFS=":$IFS"
-for dir in $*; do
-	dir="/${dir#/}"; DIR="/${opts[-root]#/}${dir}"
+for dir in ${*//:/ }; do
+	DIR="/${opts[squash-root]#/}/${dir#/}" dir="/${dir#/}"
 	if [[ -e ${DIR}.squashfs ]]; then
 		if (( ${opts[-offset]} != 0 )); then
 			rr=$(du -sk ${DIR}/rr | awk '{print $1}')
@@ -198,7 +216,7 @@ for dir in $*; do
 	fi			
 done
 
-[[ -f "$busybox" ]] && rm -f "$busybox"
+[[ -f "${busybox}" ]] && rm -f "${busybox}"
 unset DIR dir opt opts rr rw
 
 #
